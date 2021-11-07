39d01fca5b570038e80a5bcf541cc9c7d48fefe9
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2021-11-07 11:30:47 +1100

    more calc

--- src/lib/Calculator.svelte
+++ src/lib/Calculator.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
 	// import { spring } from 'svelte/motion';
 
-	let output = 0;
+	let output = "";
+	let register = [];
 
+	$: output = getRegisterOutput(r)
+
+	const getRegisterOutput = (r) => {
+		if (r.length == 0) {
+			return "0";
+		}
+		return listToNumber(r);
+	}
+
+	const listToNumber = (list) => {
+		return parseFloat(list.join(''));
+	}
+
+	const handleBtnPress = (val) => {
+		register.shift(val)
+	}
 	// const displayed_count = spring();
 	// $: displayed_count.set(count);
 	// $: offset = modulo($displayed_count, 1);
@@ -16,26 +33,26 @@
 <div class="calculator">
 	<div class="output">{output}</div>
 	<div class="row">
-		<button class="digit" on:click={() => output = 7}>7</button>
-		<button class="digit" on:click={() => output = 8}>8</button>
-		<button class="digit" on:click={() => output = 9}>9</button>
+		<button class="digit" on:click={() => handleBtnPress(7)}>7</button>
+		<button class="digit" on:click={() => handleBtnPress(8)}>8</button>
+		<button class="digit" on:click={() => handleBtnPress(9)}>9</button>
 		<button class="digit" on:click={() => null}>-</button>
 	</div>
 	<div class="row">
-		<button class="digit" on:click={() => output = 4}>4</button>
-		<button class="digit" on:click={() => output = 5}>5</button>
-		<button class="digit" on:click={() => output = 6}>6</button>
+		<button class="digit" on:click={() => handleBtnPress(4)}>4</button>
+		<button class="digit" on:click={() => handleBtnPress(5)}>5</button>
+		<button class="digit" on:click={() => handleBtnPress(6)}>6</button>
 		<button class="digit" on:click={() => null}>+</button>
 	</div>
 	<div class="row">
-		<button class="digit" on:click={() => output = 1}>1</button>
-		<button class="digit" on:click={() => output = 2}>2</button>
-		<button class="digit" on:click={() => output = 3}>3</button>
+		<button class="digit" on:click={() => handleBtnPress(1)}>1</button>
+		<button class="digit" on:click={() => handleBtnPress(2)}>2</button>
+		<button class="digit" on:click={() => handleBtnPress(3)}>3</button>
 		<button class="digit" on:click={() => null}>x</button>
 	</div>
 	<div class="row">
 		<button class="digit" on:click={() => null}></button>
-		<button class="digit" on:click={() => output = 0}>0</button>
+		<button class="digit" on:click={() => handleBtnPress(0)}>0</button>
 		<button class="digit" on:click={() => null}></button>
 		<button class="digit" on:click={() => null}>=</button>
 	</div>
